a6625819a1c3c734c133117389f079419656956b
Adriano Melquiades <apmelqui@gmail.com>
2022-06-24 19:24:13 -0400

    working on the model

--- Adriano_Melquiades-mid_term_test_02.py
+++ Adriano_Melquiades-mid_term_test_02.py
@@ -88,6 +88,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
@@ -157,77 +158,118 @@ print(y_test_adriano.shape)
 
 
 #Define an object to hold the LabelEncoder name it LR_firstname
-
 LR_adriano = LabelEncoder()
 
+#Fit/transform the y_train_firstname using the LR_firstname.
 LR_adriano.fit_transform(y_train_adriano)
 
+#LR_adriano.fit(y_train_adriano)
 
 
-
+#Prepare a pipeline to handle the numeric data columns of the feature space. 
+#Name the pipeline num_pipeline_firstname. The pipeline should handle the following steps:
+#Fill in all the missing values using the median value of each column. 
+#Apply the standard scalar.
 num_pipeline_adriano = Pipeline([
 ('imputer', SimpleImputer(strategy="median")),
 ('std_scaler', StandardScaler()),
 ])
 
 
+#3) Build, train and initial test of the model
 
-#Build, train and initial test of the model    
-
-
+# Build a SVC classifier name it clf_svm_firstname, set gamma to equal "auto" and set the 
+#random_state=last two digits of your student id.
 clf_svm_adriano = SVC(gamma='auto', random_state=57)
+#clf_svm_adriano = SVC(random_state=57)
+############################### should i put gamma='auto' above?????? #############################
+############################### should i put gamma='auto' above?????? #############################
+############################### should i put gamma='auto' above?????? #############################
+############################### should i put gamma='auto' above?????? #############################
+############################### should i put gamma='auto' above?????? #############################
+############################### should i put gamma='auto' above?????? #############################
 
+#Prepare another pipeline for the training process call it "pipe".
+#This pipeline should contain two steps, the first step is the num_pipeline_firstname 
+#and the second is the SVC classifier you defined in step#1.
 pipe = Pipeline([
         ('transformer_pipeline', num_pipeline_adriano),
         ('svc', clf_svm_adriano)
         ])
 
+#Fit the training data X_train_firstname to the pipeline "pipe" you defined in step#2.
 pipe.fit(X_train_adriano, y_train_adriano)
 
 
-# Fine tune the model 
+#Carryout 3 cross validation on the training and print out the mean of the scores. 
+#(Hint: use "pipe" instead of clf_svm_firstname)
+scores = cross_val_score(pipe, X_train_adriano, y_train_adriano, cv=3)
+mean_scores = scores.mean()
+mean_scores
 
-#Creating the grid parameters
+#Save the accuracy score of the training process to a variable named training_accuracy.
+training_accuracy = mean_scores
+
+#Use the model to predict the test data,  i.e. pass the test features to the model and 
+#save the results into a variable named initial_predictions. 
+#(Remember to transform the test data i.e. pass through the label encoder and the num_pipeline)
+adriano_predictions = pipe.predict(X_test_adriano)
+
+#Calculate the accuracy score and save it to a variable named initial_accuracy and print it out.
+initial_accuray = accuracy_score(y_test_adriano, adriano_predictions)
+print(f'The initial_accuray is {initial_accuray}')
+
+
+#4) Fine tune the model
+
+#Use grid search to fine tune the parameters of the SVM estimator, setup the following parameters:
+#kernel: ['linear','rbf']
+#C: [0.01, 0.1, 1]
+#gamma: [0.01,0.06, 0.1]
 param_grid = [
-    {'svc__kernel': ['linear', 'rbf'],
-     'svc__C': [0.01, 0.1, 1],
+    {'svc__kernel': ['linear', 'rbf'], 
+     'svc__C': [0.01, 0.1, 1], 
      'svc__gamma': [0.01, 0.06, 0.1]}]
-    
-print(param_grid)
 
-#Creating a grid search object
-grid_search_adriano = GridSearchCV(estimator=pipe, 
+
+
+#Apply GridSearchCV on the training data. Set the estimator as clf_svm_firstname, and use accuracy 
+#as a scoring method, also set refit to 'True' and verbose  to 3.
+grid_search_adriano = GridSearchCV(estimator=clf_svm_adriano, 
                                    param_grid=param_grid, 
                                    scoring='accuracy', 
                                    refit=True, 
                                    verbose=3)
-grid_search_adriano
 
-#Fitting the data into the grid search object
 grid_search_adriano.fit(X_train_adriano, y_train_adriano)
 
-#Retreiving the best parameters and best estimator
+
+#Printout the best parameters 
 grid_search_adriano.best_params_
 grid_search_adriano.best_estimator_
 
-#Creating the best model (with the best fine-tuned model) to predict the test data 
+
+#5) Test & save the model
+
+#Save the best model the grid search generated to a variable and name it  best_model_firstname. 
+#(Hint: check grid_search.best_estimator_ )
 best_model_adriano = grid_search_adriano.best_estimator_
-best_model_adriano
 
+#Predict the test data using the best model,  i.e. pass the test features to the model and save the 
+#results into a variable named final_predictions.
+final_predictions = best_model_adriano.predict(X_test_adriano)
 
+#Calculate the accuracy score and save it to a variable named final_accuracy and print it out.
+final_predictions_accuracy = best_model_adriano.score(X_test_adriano, final_predictions)
+print(f'The score of the final predictions is {final_predictions_accuracy}')
 
-############################
+#Record all three  accuracy scores in the below box and compare them and draw a conclusion.???
 
-from sklearn import cross_val_score
-mean_score = cross_val_score(clf_svm_adriano, X_train_adriano, y_train_adriano, cv=3).mean()
+#Save the best model to your hard disk name the object best_model_firstname.pkl and attach it to your submission.
+joblib.dump(final_predictions, 'best_model_adriano.pkl')
 
-
-
-#Saving the model using joblib
-joblib.dump(best_model_adriano, 'SVC_model_adriano.pkl')
-
-#Saving the full pipeline using joblib
-joblib.dump(pipe_svm_adriano, 'full_pipeline_adriano.pkl')
-
-
+#Save both the label encoder LR_firstname and the transformation pipeline "num_pipeline_firstname" to the disk, 
+#and attach the objects to your submission.(Hint: Use joblib dump)
+joblib.dump(LR_adriano, 'LR_adriano.pkl')
+joblib.dump(num_pipeline_adriano, 'num_pipeline_adriano.pkl')
 
